d88a8553256f9db838e45ae88d25c9f4be18813b
chenjiajin <1416978127@qq.com>
2018-07-04 12:10:07 +0800

    修改一些配置和controller的入参接收

--- src/main/java/com/microservice/basecommonsvr/controller/RedisController.java
+++ src/main/java/com/microservice/basecommonsvr/controller/RedisController.java
@@ -1,7 +1,9 @@
 package com.microservice.basecommonsvr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lilanz.microservice.common.entity.Result;
@@ -14,12 +16,17 @@ public class RedisController {
 	private ReqRecordService reqRecordService;
 	
 	@PostMapping("/redisInsertInter")
-	private Result<?> redisInsertInter(String appName,String reqid,int tid){
+	private Result<?> redisInsertInter(@RequestParam String appName,@RequestParam String reqid,@RequestParam int tid){
 		return reqRecordService.redisInsertInter(appName, reqid, tid);
 	}
 	
 	@PostMapping("/redisUpdateInter")
-	private Result<?> redisUpdateInter(String appName,String reqid,int tid,boolean status){
+	private Result<?> redisUpdateInter(@RequestParam String appName,@RequestParam String reqid,@RequestParam int tid,@RequestParam boolean status){
 		return reqRecordService.redisUpdateInter(appName, reqid, tid,status);
+	}
+	
+	@GetMapping("/myinfo")
+	private String Myinfo() {
+		return "Redis";
 	}
 }
